db84d820623edf93290572c343846be400809883
ozPop <ozziepopovas@gmail.com>
2016-10-18 12:13:24 -0400

    update calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,16 +1,59 @@
 function doMath() {
   $('#equals').on('click', function() {
-    collectInputs(); // returns an object
+    let inputs = collectInputs(); // returns an object
+
+    // Check if all fields are filled
+    for (var key in inputs) {
+      if (inputs[key] === "") {
+        console.log("Please fill out the fields");
+        return;
+      }
+    }
+
+    let check = validate(inputs.operation, inputs.number1, inputs.number2);
+    if (check === true) {
+      console.log(result(inputs.operation, inputs.number1, inputs.number2));   
+    } else {
+      console.log(check);
+    }
   });
 }
 
 function collectInputs() {
-  var values = {};
+  var inputs = {};
   $.each($('div input'), function(index, element) {
     let label = $(this).prev().attr('for');
-    values[label] = $(this).val();
+    inputs[label] = $(this).val();
   });
-  return values;
+  return inputs;
+}
+
+function validate(operator, num1, num2) {
+  const validOperators = ['+', '-', '*', '/'];
+  if (!validOperators.includes(operator)) {
+    return "Sorry, that is not a valid operator";
+  } else if (!parseInt(num1) || !parseInt(num2)) {
+    return "Sorry, one of those is not a valid number!";
+  } else {
+    return true;
+  }
+}
+
+function result(operator, num1, num2) {
+  return calculate(operator, num1, num2);
+}
+
+function calculate(operator, num1, num2) {
+  switch(true) {
+    case operator === "+":
+      return parseInt(num1) + parseInt(num2);
+    case operator === "-":
+      return parseInt(num1) - parseInt(num2);
+    case operator === "*":
+      return parseInt(num1) * parseInt(num2);
+    case operator === "/":
+      return parseInt(num1) / parseInt(num2);
+  }
 }
 
 $(document).ready(function() {
