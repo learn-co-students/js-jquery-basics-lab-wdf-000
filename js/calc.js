7616f8587d80ba2f3f50d949af31cc2a3902ac4a
Bibi Khadiza <limakhadiza@yahoo.com>
2016-11-01 13:31:05 -0400

    added notes

--- js/calc.js
+++ js/calc.js
@@ -14,6 +14,15 @@ function doMath(){
   });
 }
 
+//in this method you are first calling a function which does not take any arguments
+//you use a jQuery selector to select the values of the input fileds of id numer1 and number 2
+// along with the operator. you then pass the var = t the values to the validate function
+//remember this is a method dealing with an event handler because the user triggered an action and the
+//method is responding to that action.
+//"click" is the event handler which is binded with the selector and triggers the function with all the vars
+
+
+
 function validate(operator, num1, num2){
   var num1 = parseInt(num1);
   var num2 = parseInt(num2);
@@ -26,8 +35,24 @@ function validate(operator, num1, num2){
 
 }
 
+
+// this validates function takes in the inputed oeprator and two nums
+//we parse the strings into integer
+//if the iperator is not equal to any of the given then we return an error message
+//if the numer is not valid aka isNan is false . then we return the error
+//else we pass the parameters to the result function which calculates the sum
+
+
+
+
 function result(operator, num1, num2){
   var result = eval(num1 + operator + num2);
   $('#result').text(result);
   return result;
-} 
+
+}
+
+
+//the function result takes in 3 parameters all validated. the eval function in javascript executes code expressions
+//it evaluates the expresstion and then we select the result html with the jquery selector and add the jquery method which allows
+//us to manipulate the output but displaying the result in that result tag. we then return the var result.
