598c6c81b0687ee27fee327b7910321794afc56f
dimitriwashere <dimitriwashere@gmail.com>
2017-06-20 16:41:51 -0400

    Done.

---
+++ js/calc.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  doMath();
+})
+
+var number1;
+var number2;
+var operation;
+
+function doMath() {
+  $('#equals').on('click', function(){
+    number1 = parseInt($('#number1').val());
+    number2 = parseInt($('#number2').val());
+    operation = $('#operation').val();
+
+    validate(operation, number1, number2)
+  })
+}
+
+function validate(op, num1, num2){
+  if (isNaN(num1) || isNaN(num2) ){
+    return $('#result').empty().append("Sorry, one of those is not a valid number!");
+  } else if (op != "+" && op != "-" && op != "*" && op != "/"){
+    return $('#result').empty().append("Sorry, that is not a valid operator");
+  } else {
+  return result(op, num1, num2);
+  }
+}
+
+function result(op, num1, num2){
+  var result;
+  switch(op) {
+      case "+":
+          result = num1 + num2;
+          break;
+      case "-":
+          result = num1 - num2;
+          break;
+      case "*":
+          result = num1 * num2;
+          break;
+      case "/":
+          result = num1 / num2;
+          break;
+  }
+  $('#result').empty().append(result);
+  return result;
+}
